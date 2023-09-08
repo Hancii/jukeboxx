4dadea34272b9d9b27dbdd5f365da12268fd031a
Hancii <hlendzer1@etf.unsa.ba>
2023-09-08 16:55:51 +0200

    ..

--- src/main/java/ba/unsa/etf/rpr/domain/Singer.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Singer.java
@@ -1,4 +1,42 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Singer {
+import java.util.Objects;
+
+public class Singer implements Idable{
+
+    private int id;
+
+    private String singer;
+
+    private Category category;
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public String getSinger() {return singer;}
+
+    public void setSinger(String singer) {this.singer = singer;}
+
+    public Category getCategory() {return category;}
+
+    public void setCategory(Category category) {this.category = category;}
+
+    @Override
+    public String toString() {
+        return "nesta";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Singer singer = (Singer) o;
+        return id == singer.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, singer, category);
+    }
 }
