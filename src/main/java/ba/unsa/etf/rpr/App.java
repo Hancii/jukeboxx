966f2cca93e5d7c34b252348b78ea16591ba2d89
Hancii <hlendzer1@etf.unsa.ba>
2023-09-08 16:21:48 +0200

    ..

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -6,9 +6,7 @@ package ba.unsa.etf.rpr;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!"
-        );
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
     }
 }
