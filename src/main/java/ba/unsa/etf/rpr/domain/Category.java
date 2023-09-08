f0a6651b1e1cc5d50892d79ebf7e94a152f7c9eb
Hancii <hlendzer1@etf.unsa.ba>
2023-09-08 16:45:13 +0200

    ..

--- src/main/java/ba/unsa/etf/rpr/domain/Category.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Category.java
@@ -2,5 +2,31 @@ package ba.unsa.etf.rpr.domain;
 
 import java.util.Objects;
 
-public class Category {
+public  class Category implements Idable{
+
+    private int id;
+
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Category() {}
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getString() {
+        return name;
+    }
+
 }
