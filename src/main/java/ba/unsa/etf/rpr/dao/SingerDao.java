ca5b10d3c29465dba6bec4f450dcf5f2e3f506ab
Hancii <hlendzer1@etf.unsa.ba>
2023-09-08 17:12:42 +0200

    ..

---
+++ src/main/java/ba/unsa/etf/rpr/dao/SingerDao.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Category;
+import ba.unsa.etf.rpr.domain.Singer;
+import ba.unsa.etf.rpr.exception.JukeBoxException;
+
+import java.util.List;
+
+public interface SingerDao extends Dao<Singer> {
+
+    List<Singer> searchByText(String text) throws JukeBoxException;
+}
