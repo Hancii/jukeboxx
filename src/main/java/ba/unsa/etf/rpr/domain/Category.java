ca62b5af0f5dd02c27c6f220ebfe9b61af8b52ec
Hancii <hlendzer1@etf.unsa.ba>
2023-09-08 16:46:46 +0200

    ..

--- src/main/java/ba/unsa/etf/rpr/domain/Category.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Category.java
@@ -29,4 +29,21 @@ public  class Category implements Idable{
         return name;
     }
 
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
