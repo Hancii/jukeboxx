f958154431ffb8550885fed8b6b4a6bbda02aa9f
Hancii <hlendzer1@etf.unsa.ba>
2023-09-12 18:46:11 +0200

    Implemented SingerDaoSQLImpl to handle database table operations on categories

---
+++ src/main/java/ba/unsa/etf/rpr/dao/SingerDaoSQLImpl.java
@@ -0,0 +1,59 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Category;
+import ba.unsa.etf.rpr.domain.Singer;
+import ba.unsa.etf.rpr.exception.JukeBoxException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class SingerDaoSQLImpl extends AbstractDao<Singer> implements SingerDao {
+    private static SingerDaoSQLImpl instance = null;
+
+    private SingerDaoSQLImpl() {
+        super("singer");
+    }
+
+    public static SingerDao getInstance() {
+        if (instance == null) {
+            instance = new SingerDaoSQLImpl();
+        }
+        return instance;
+    }
+
+    @Override
+    public Singer row2object(ResultSet rs) throws JukeBoxException {
+        try {
+            Singer singer = new Singer();
+            singer.setId(rs.getInt("id"));
+            singer.setName(rs.getString("name"));
+            singer.setCategory(DaoFactory.categoryDao().getById(rs.getInt("category_id")));
+            return singer;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new JukeBoxException("Cannot convert Category row to object.", e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> object2row(Singer singer) {
+        Map<String, Object> row = new TreeMap<>();
+        row.put("id", singer.getId());
+        row.put("name", singer.getName());
+        row.put("category_id", singer.getCategory().getId());
+        return row;
+    }
+
+    @Override
+    public List<Singer> searchByText(String text) throws JukeBoxException {
+        return null;
+    }
+
+    @Override
+    public List<Singer> searchByCategory(Category category) throws JukeBoxException {
+        return null;
+    }
+}
