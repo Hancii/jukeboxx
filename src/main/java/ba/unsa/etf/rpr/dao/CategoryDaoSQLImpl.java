964fb76f8c70e87263224a302b528623a48c874b
Hancii <hlendzer1@etf.unsa.ba>
2023-09-19 07:03:03 +0200

    categorydao - row2object

--- src/main/java/ba/unsa/etf/rpr/dao/CategoryDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CategoryDaoSQLImpl.java
@@ -23,5 +23,25 @@ public class CategoryDaoSQLImpl extends AbstractDao<Category> implements Categor
         return instance;
     }
 
+    /**
+     * Converts results set into Category object by reading appropriate columns
+     *
+     * @param  rs the result set to convert
+     * @return      category that is converted from result set
+     * */
+    @Override
+    public Category row2object(ResultSet rs) throws JukeBoxException {
+        try {
+            Category category = new Category();
+            category.setId(rs.getInt("id")); // "id"
+            category.setName(rs.getString("name"));
+            return category;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new JukeBoxException("Cannot convert Category row to object.", e);
+        }
+    }
+
+
 
 }
