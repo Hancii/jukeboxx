cc507871797b04b9ddbd981c6fe780e513cd10b2
Hancii <hlendzer1@etf.unsa.ba>
2023-09-19 06:52:14 +0200

    abstract Dao - createConnection

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -10,6 +10,37 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T> {
     private static Connection connection = null;
     protected final String tableName;
 
+    public AbstractDao(String tableName) {
+        this.tableName = tableName;
+        createConnection();
+    }
+
+    private static void createConnection() {
+        if (AbstractDao.connection == null) {
+            try {
+                Properties p = new Properties();
+                p.load(ClassLoader.getSystemResource("application.properties").openStream());
+                String url = p.getProperty("db.connection_string");
+                String username = p.getProperty("db.username");
+                String password = p.getProperty("db.password");
+                AbstractDao.connection = DriverManager.getConnection(url, username, password);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                Runtime.getRuntime().addShutdownHook(new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            connection.close();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
 
 }
 
