8d2b6c85df96f073767d9ed255f9237e31931fd2
Hancii <hlendzer1@etf.unsa.ba>
2023-09-19 07:04:08 +0200

    SongDao -

--- src/main/java/ba/unsa/etf/rpr/dao/SongDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/SongDao.java
@@ -1,11 +1,12 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Singer;
 import ba.unsa.etf.rpr.domain.Song;
 import ba.unsa.etf.rpr.exception.JukeBoxException;
 
 import java.util.List;
 
-public interface SongDao {
+public interface SongDao extends Dao<Song> {
 
     List<Song> getByNameLink(String name, String link) throws JukeBoxException;
 }
