483be073931e09d007cff3a09cbb8db31096c13c
Hancii <hlendzer1@etf.unsa.ba>
2023-09-19 07:05:47 +0200

    Singerdao - row2object

--- src/main/java/ba/unsa/etf/rpr/dao/SingerDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/SingerDaoSQLImpl.java
@@ -23,6 +23,25 @@ public class SingerDaoSQLImpl extends AbstractDao<Singer> implements SingerDao {
         }
         return instance;
     }
+    /**
+     * Converts results set into Singer object by reading appropriate columns
+     *
+     * @param  rs the result set to convert
+     * @return      category that is converted from result set
+     * */
+    @Override
+    public Singer row2object(ResultSet rs) throws JukeBoxException {
+        try {
+            Singer singer = new Singer();
+            singer.setId(rs.getInt("id"));
+            singer.setName(rs.getString("name"));
+            singer.setCategory(DaoFactory.categoryDao().getById(rs.getInt("category_id")));
+            return singer;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new JukeBoxException("Cannot convert Category row to object.", e);
+        }
+    }
 
 
 }
