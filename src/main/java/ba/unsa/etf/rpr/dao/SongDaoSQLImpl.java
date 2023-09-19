ea0961b62e4312c984e8ea32d9faaaf4c843ca07
Hancii <hlendzer1@etf.unsa.ba>
2023-09-19 07:08:05 +0200

    SongDao - singleton

--- src/main/java/ba/unsa/etf/rpr/dao/SongDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/SongDaoSQLImpl.java
@@ -1,6 +1,5 @@
 package ba.unsa.etf.rpr.dao;
 
-import ba.unsa.etf.rpr.domain.Singer;
 import ba.unsa.etf.rpr.domain.Song;
 import ba.unsa.etf.rpr.exception.JukeBoxException;
 
@@ -25,32 +24,5 @@ public class SongDaoSQLImpl extends AbstractDao<Song> implements SongDao {
         return instance;
     }
 
-    @Override
-    public Song row2object(ResultSet rs) throws JukeBoxException {
-        try {
-            Song song = new Song();
-            song.setId(rs.getInt("id"));
-            song.setName(rs.getString("name"));
-            song.setLink(rs.getString("link"));
-            song.setSinger(DaoFactory.singerDao().getById(rs.getInt("singer_id")));
-            return song;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new JukeBoxException("Cannot convert Song row to object.", e);
-        }
-    }
 
-    @Override
-    public Map<String, Object> object2row(Song song) throws JukeBoxException {
-        Map<String, Object> row = new TreeMap<>();
-        row.put("id", song.getId());
-        row.put("name", song.getName());
-        row.put("singer_id", song.getSinger().getId());
-        return row;
-    }
-
-    @Override
-    public List<Song> getByNameLink(String name, String link) throws JukeBoxException {
-        return null;
-    }
 }
